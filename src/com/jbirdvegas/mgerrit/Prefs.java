b712e00353455373818493050336f6e28899b74a
p4r4n01d <p4r4n01d42@gmail.com>
2013-08-08 14:28:34 +1000

    Implement Up navigation
    Implement Up Affordance (Up Navigation) on all the child activities
    Add support library
    Remove gradle cache files

--- src/com/jbirdvegas/mgerrit/Prefs.java
+++ src/com/jbirdvegas/mgerrit/Prefs.java
@@ -26,6 +26,8 @@ import android.preference.ListPreference;
 import android.preference.Preference;
 import android.preference.PreferenceActivity;
 import android.preference.PreferenceManager;
+import android.support.v4.app.NavUtils;
+import android.view.MenuItem;
 import android.widget.Toast;
 import com.jbirdvegas.mgerrit.objects.CommitterObject;
 
@@ -56,6 +58,10 @@ public class Prefs extends PreferenceActivity implements Preference.OnPreference
         findPreference(AOSP_VOLLEY).setOnPreferenceClickListener(this);
         // View Apache Commons Codec
         findPreference(APACHE_COMMONS_KEY).setOnPreferenceClickListener(this);
+
+        // Action bar Up affordance
+        getActionBar().setDisplayHomeAsUpEnabled(true);
+
         // select gerrit instance
         ListPreference gerritList = (ListPreference) findPreference(GERRIT_KEY);
         gerritList.setSummary(gerritList.getValue());
@@ -171,5 +177,16 @@ public class Prefs extends PreferenceActivity implements Preference.OnPreference
     public static TimeZone getLocalTimeZone(Context context) {
         return TimeZone.getTimeZone(PreferenceManager.getDefaultSharedPreferences(context)
                 .getString(LOCAL_TIMEZONE_KEY, TimeZone.getDefault().getID()));
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            // Respond to the action bar's Up/Home button
+            case android.R.id.home:
+                NavUtils.navigateUpFromSameTask(this);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
