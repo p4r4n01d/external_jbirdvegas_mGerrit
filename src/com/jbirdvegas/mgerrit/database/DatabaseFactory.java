2938aa2af886dd166c5e67059d168d3dbd29df6d
p4r4n01d <p4r4n01d42@gmail.com>
2013-09-06 10:12:54 +1000

    Allow removing of followed projects and authors
    Previously swiping was used to remove watched items, however
    since ViewPager steals all click actions something new was needed.
    Cards that represent the stalking of a project/user have a " X "
    button users can use to remove the stalking quality when they
    no longer care about a project/user.
    
    Add new idea to TODO list
    Fix some database errors
    
    PS2: Remove redundant linear layout
    PS3: Remove unneeded drawable
       : Fix create on date in ProjectCard
    
    Change-Id: Icf19608a2ec9e4bfcc827cf14b00613cf981d149

--- src/com/jbirdvegas/mgerrit/database/DatabaseFactory.java
+++ src/com/jbirdvegas/mgerrit/database/DatabaseFactory.java
@@ -1,5 +1,22 @@
 package com.jbirdvegas.mgerrit.database;
 
+/*
+ * Copyright (C) 2013 Android Open Kang Project (AOKP)
+ *  Author: Evan Conway (P4R4N01D), 2013
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
@@ -30,16 +47,14 @@ public class DatabaseFactory
     public static DatabaseFactory getDatabase(Context context, String gerrit)
     {
         String dbName = DBHelper.getDatabaseName(gerrit);
-        DatabaseFactory.dbHelper = DBHelper.getInstance(context, dbName);
-
         if (mInstance != null && dbName.equals(mCurrentGerrit)) return mInstance;
+        mInstance = new DatabaseFactory();
+        mInstance.mContext = context;
+        DatabaseFactory.dbHelper = DBHelper.getInstance(mInstance, context, dbName);
 
         // Ensure the database is open and we have a reference to it before
         //  trying to perform any queries using it.
         DatabaseFactory.wdb = dbHelper.getWritableDatabase();
-
-        mInstance = new DatabaseFactory();
-        mInstance.mContext = context;
         return mInstance;
     }
 
