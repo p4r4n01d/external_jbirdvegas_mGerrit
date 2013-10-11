75570d4f145b9357779ddef96ca93ba1699a13a7
Evan Conway <p4r4n01d42@gmail.com>
2013-10-11 11:15:43 +1100

    Move code review text into strings resource.
    Change-Id: I510a3d8e0d4600d150f0fd77dd65fd6f68876e39

--- src/com/jbirdvegas/mgerrit/objects/Reviewer.java
+++ src/com/jbirdvegas/mgerrit/objects/Reviewer.java
@@ -24,12 +24,6 @@ import com.google.gson.annotations.SerializedName;
 
 public class Reviewer implements Parcelable {
     public static final String NO_SCORE = "No score";
-    public static final String CODE_REVIEW_PLUS_TWO = "Looks good to me, approved";
-    public static final String CODE_REVIEW_PLUS_ONE = "Looks good to me, but someone else must approve";
-    public static final String CODE_REVIEW_MINUS_ONE = "I would prefer that you didn\u0027t submit this";
-    public static final String CODE_REVIEW_MINUS_TWO = "Do not submit";
-    public static final String VERIFIED_PLUS_ONE = "Verified";
-    public static final String VERIFIED_MINUS_ONE = "Fails";
 
     @SerializedName("value")
     private String mValue;
