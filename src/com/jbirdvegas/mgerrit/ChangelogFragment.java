f9a375619c461f4c917007417b8e178e0471c466
Evan Conway <p4r4n01d42@gmail.com>
2014-01-30 14:15:17 +1100

    Changelog: Detect no builds available
    AOKP Changelog: Show message when no builds are
     available for the device.
    Improve error checking around getting the change to
     select from the database.
    
    Change-Id: If64fe389157999a2132190dccfa0e49e120250c0

--- src/com/jbirdvegas/mgerrit/ChangelogFragment.java
+++ src/com/jbirdvegas/mgerrit/ChangelogFragment.java
@@ -29,6 +29,7 @@ import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ImageView;
 import android.widget.Spinner;
+import android.widget.TextView;
 import android.widget.Toast;
 import android.widget.ViewSwitcher;
 
@@ -142,23 +143,29 @@ public class ChangelogFragment extends Fragment {
     class gooImResponseListener implements Response.Listener<JSONObject> {
         @Override
         public void onResponse(JSONObject response) {
+            JSONArray result = response.optJSONArray("list");
+            if (result == null) {
+                Log.w(TAG, mParent.getString(R.string.no_changelog));
+                TextView text = (TextView) mParent.findViewById(R.id.changelog_searching);
+                text.setText("There are no recent builds for your device");
+                mParent.findViewById(R.id.changelog_progressBar).setVisibility(View.GONE);
+                return;
+            }
             mViewSwitcher.showNext();
-
             Toast.makeText(mParent,
                     R.string.please_select_update_for_range,
                     Toast.LENGTH_LONG).show();
-            try {
-                JSONArray result = response.getJSONArray("list");
-                int resultsSize = result.length();
-                List<GooFileObject> filesList = new LinkedList<>();
 
+            int resultsSize = result.length();
+            List<GooFileObject> filesList = new LinkedList<>();
+            try {
                 for (int i = 0; resultsSize > i; i++) {
                     filesList.add(GooFileObject.getInstance(result.getJSONObject(i)));
                 }
-                setupList(filesList);
             } catch (Exception e) {
                 Log.e("ErrorListener", e.getLocalizedMessage());
             }
+            setupList(filesList);
         }
     }
 
