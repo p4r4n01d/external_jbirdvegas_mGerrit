e8fd4b933012257087ca614e9291cc297cd5e0c1
p4r4n01d <p4r4n01d42@gmail.com>
2014-03-30 17:16:23 +1100

    WIP: SwipeRefresh theming
    Change-Id: I12f79cdf70b20c5a35f4c92b7a55833e8659ba2a

--- src/com/jbirdvegas/mgerrit/views/ThemeableSwipeRefreshLayout.java
+++ src/com/jbirdvegas/mgerrit/views/ThemeableSwipeRefreshLayout.java
@@ -5,6 +5,7 @@ import android.content.res.TypedArray;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.util.AttributeSet;
 
+import com.jbirdvegas.mgerrit.Prefs;
 import com.jbirdvegas.mgerrit.R;
 
 public class ThemeableSwipeRefreshLayout extends SwipeRefreshLayout {
@@ -12,13 +13,20 @@ public class ThemeableSwipeRefreshLayout extends SwipeRefreshLayout {
     public ThemeableSwipeRefreshLayout(Context context, AttributeSet attrs) {
         super(context, attrs);
 
-        TypedArray a = context.obtainStyledAttributes(attrs, R.styleable.ThemeableSwipeRefreshLayout,
-                0, R.style.RefreshView);
+        int theme = Prefs.getCurrentThemeID(context);
+        // The attributes you want retrieved
+        int[] theAttrs = {R.attr.refreshColor1, R.attr.color2, R.attr.color3, R.attr.color4};
 
-        int color1 = a.getColor(R.styleable.ThemeableSwipeRefreshLayout_color1, R.color.card_background);
-        int color2 = a.getColor(R.styleable.ThemeableSwipeRefreshLayout_color2, R.color.card_background);
-        int color3 = a.getColor(R.styleable.ThemeableSwipeRefreshLayout_color3, R.color.card_background);
-        int color4 = a.getColor(R.styleable.ThemeableSwipeRefreshLayout_color4, R.color.card_background);
+        int style;
+        if (theme == R.style.Theme_Light) style = R.style.RefreshView;
+        else style = R.style.RefreshView_Dark;
+
+        TypedArray a = context.obtainStyledAttributes(style, theAttrs);
+
+        int color1 = a.getColor(0, R.color.card_background);
+        int color2 = a.getColor(1, R.color.card_background);
+        int color3 = a.getColor(2, R.color.card_background);
+        int color4 = a.getColor(3, R.color.card_background);
 
         setColorScheme(color1, color2, color3, color4);
         a.recycle();
