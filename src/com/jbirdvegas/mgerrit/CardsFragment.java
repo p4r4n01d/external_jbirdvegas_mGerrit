aa429cbe4066acba1d539c7210f8cd29a7c89ea9
p4r4n01d <p4r4n01d42@gmail.com>
2015-01-02 11:43:14 +1100

    Add sticky headers to change lists
    Add headers with the date in each of the commit lists.
    This uses a new StickyListHeaders library
    Update the database to fix possible sort key crash
    BUGS:
    - The adapter is not properly notified when we scroll to the bottom to load more content
    - Switching tabs does not load list content. Content only refreshes when toggling search
    
    Change-Id: I97ae0ddd4549fb5d39bad0d4b8dc14c2e7014fc0

--- src/com/jbirdvegas/mgerrit/CardsFragment.java
+++ src/com/jbirdvegas/mgerrit/CardsFragment.java
@@ -35,11 +35,12 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.BaseAdapter;
-import android.widget.ListView;
 
 import com.android.volley.RequestQueue;
 import com.android.volley.toolbox.Volley;
 import com.google.analytics.tracking.android.EasyTracker;
+import com.jbirdvegas.mgerrit.adapters.HeaderAdapterDecorator;
+import com.jbirdvegas.mgerrit.adapters.HeaderAdapterWrapper;
 import com.nhaarman.listviewanimations.appearance.SingleAnimationAdapter;
 import com.jbirdvegas.mgerrit.adapters.ChangeListAdapter;
 import com.jbirdvegas.mgerrit.adapters.EndlessAdapterWrapper;
@@ -69,6 +70,8 @@ import java.util.ArrayList;
 import java.util.Set;
 
 import de.greenrobot.event.EventBus;
+import se.emilsjolander.stickylistheaders.ExpandableStickyListHeadersListView;
+import se.emilsjolander.stickylistheaders.StickyListHeadersAdapter;
 
 public abstract class CardsFragment extends Fragment
         implements LoaderManager.LoaderCallbacks<Cursor> {
@@ -89,7 +92,7 @@ public abstract class CardsFragment extends Fragment
 
     private static boolean sIsTabletMode = false;
 
-    private ListView mListView;
+    private ExpandableStickyListHeadersListView mListView;
     // Adapter that binds data to the listview
     private ChangeListAdapter mAdapter;
     // Wrapper for mAdapter, enabling animations
@@ -109,6 +112,8 @@ public abstract class CardsFragment extends Fragment
         }
     };
     private EventBus mEventBus;
+    private StickyListHeadersAdapter mHeaderAdapterWrapper;
+    private HeaderAdapterWrapper mHeaderAdapter;
 
 
     @Override
@@ -153,14 +158,16 @@ public abstract class CardsFragment extends Fragment
         String[] from = new String[] { UserChanges.C_SUBJECT, UserChanges.C_NAME,
                 UserChanges.C_PROJECT, UserChanges.C_UPDATED, UserChanges.C_STATUS };
 
-        mListView = (ListView) mCurrentFragment.findViewById(R.id.commit_cards);
+        mListView = (ExpandableStickyListHeadersListView) mCurrentFragment.findViewById(R.id.commit_cards);
         registerForContextMenu(mListView);
 
         mAdapter = new ChangeListAdapter(mParent, R.layout.commit_card, null, from, to, 0,
                 getQuery());
         mAdapter.setViewBinder(new CommitCardBinder(mParent, mRequestQueue));
 
-        mEndlessAdapter = new EndlessAdapterWrapper(mParent, mAdapter) {
+        mHeaderAdapter = new HeaderAdapterWrapper(mParent, mAdapter);
+
+        mEndlessAdapter = new EndlessAdapterWrapper(mParent, mHeaderAdapter) {
             @Override
             public void loadData() {
                 Set<SearchKeyword> keywords = mSearchView.getLastQuery();
@@ -180,6 +187,10 @@ public abstract class CardsFragment extends Fragment
                 mAdapter.itemClickListener(view);
             }
         });
+
+        mHeaderAdapterWrapper = new HeaderAdapterDecorator(mEndlessAdapter, mHeaderAdapter);
+        mListView.setAdapter(mHeaderAdapterWrapper);
+        mListView.setDrawingListUnderStickyHeader(false);
 
         sChangesLimit = mParent.getResources().getInteger(R.integer.changes_limit);
 
@@ -210,7 +221,7 @@ public abstract class CardsFragment extends Fragment
         if (mEndlessAdapter != null) {
             toggleAnimations(mEndlessAdapter);
             mListView.setOnScrollListener(mEndlessAdapter);
-        } else toggleAnimations(mAdapter);
+        } else toggleAnimations(mHeaderAdapter);
 
         EasyTracker.getInstance(getActivity()).activityStart(getActivity());
 
@@ -337,7 +348,7 @@ public abstract class CardsFragment extends Fragment
         String webAddress = (String) targetView.getTag(R.id.webAddress);
         switch (item.getItemId()) {
             case R.id.menu_change_details:
-                mListView.performItemClick(targetView, info.position, info.id);
+                mListView.getWrappedList().performItemClick(targetView, info.position, info.id);
                 return true;
             case R.id.menu_change_browser:
                 Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(webAddress));
@@ -373,7 +384,7 @@ public abstract class CardsFragment extends Fragment
 
         /* If animations have been enabled, setup and use an animation adapter, otherwise use
          *  the regular adapter. The data should always be bound to mAdapter */
-        BaseAdapter adapter = Tools.toggleAnimations(mAnimationsEnabled, mListView, mAnimAdapter, baseAdapter);
+        BaseAdapter adapter = Tools.toggleAnimations(mAnimationsEnabled, mListView.getWrappedList(), mAnimAdapter, baseAdapter);
 
         if (mAnimationsEnabled) {
             mAnimAdapter = (SingleAnimationAdapter) adapter;
